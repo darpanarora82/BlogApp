3b404a134fdb58bab4d5dc22bccdccc489984b9c
Chirag Arora <darpanarora82@gmail.com>
2023-06-18 17:31:02 +0530

    Comment Feature Developed

--- src/main/java/com/blogapp/Blogapp/controller/CommentController.java
+++ src/main/java/com/blogapp/Blogapp/controller/CommentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class CommentController {
 
+    @Autowired
     public CommentService commentService;
 
     public CommentController(CommentService commentService) {
